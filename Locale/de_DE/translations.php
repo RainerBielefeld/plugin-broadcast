f2799fd94b8f1a9a636075bc81607b095fe31a12
RainerBielefeld <RainerBielefeldNG@bielefeldundbuss.de>
2024-04-14 09:41:57 +0200

    Add files via upload
    Add German and Spanish Locale

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Broadcast Message' => 'Nachricht an alle',
+    'Announcement' => 'Ankündigung',
+    'Clear message' => 'Nachricht löschen',
+    'Publish' => 'Senden',
+    'Broadcast Message by Email' => 'Email-Nachricht an alle',
+    'Send to everyone' => 'An alle senden',
+];
---
+++ Locale/es_ES/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'Broadcast Message' => 'Mensaje de difusión',
+    'Announcement' => 'Convocatoria',
+    'Clear message' => 'Borrar mensaje',
+    'Publish' => 'Enviar este mensaje',
+    'Broadcast Message by Email' => 'Difusión de mensaje por correo electrónico',
+    'Send to everyone' => 'Enviar a todos',
+];
